618e45512d14f600c5b22267e5d2e2059e163816
Hiroshi Miura <miurahr@linux.com>
2021-04-06 00:23:12 +0900

    Update release script
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- release.sh
+++ release.sh
@@ -2,6 +2,9 @@
 git checkout releases
 git merge master
 npm install
+npm run build
 npm i -g @vercel/ncc
 ncc build lib/main.js --license license.txt
-git add dist
+git add action.yml dist/index.js
+git commit -sm 'Update release'
+git push origin releases
