5e8a26a4eac99f0e8213fb68450e69c6435c644a
Hiroshi Miura <miurahr@linux.com>
2021-04-10 06:55:39 +0900

    Bump versions
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 git checkout releases
 git merge -m 'merge master into releases' master
 npm install
+npm install -g webpack
 npm install -g @vercel/ncc
 ncc build lib/main.js --license license.txt
 git add action.yml dist/index.js
