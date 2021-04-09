4c344f41abb2d08e31fe348833c522c7b5a37ef4
Hiroshi Miura <miurahr@linux.com>
2021-04-10 07:25:52 +0900

    Update release and test procedure
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- release.sh
+++ release.sh
@@ -2,9 +2,10 @@
 git checkout releases
 git merge -m 'merge master into releases' master
 npm install
+npm install -g typescript
 npm install -g webpack
 npm install -g @vercel/ncc
-ncc build lib/main.js --license license.txt
+npm run all
 git add action.yml dist/index.js
 git commit -sm 'Update release'
 git push origin releases
