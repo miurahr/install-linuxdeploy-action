46ebb0bfac5737f6f9ae6eac029b497af132153e
Hiroshi Miura <miurahr@linux.com>
2021-04-10 08:26:48 +0900

    Drop old tsc from package.json
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import * as process from 'process'
-import * as core from '@actions/core'
-import * as exec from '@actions/exec'
-import dotenv from 'dotenv';
+import * as process from 'process';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-async function install_target(target_base, name, targetdir): Promise<void> {
+const install_target = async (target_base, name, targetdir): Promise<void> => {
   try {
-    const target: string = target_base.concat(name)
-    const executable: string = targetdir.concat('/', name)
-    await exec.exec(`wget -c -nv ${target} -O ${executable}`)
-    await exec.exec(`chmod +x ${executable}`)
-    core.addPath(targetdir)
+    const target: string = target_base.concat(name);
+    const executable: string = targetdir.concat('/', name);
+    await exec.exec(`wget -c -nv ${target} -O ${executable}`);
+    await exec.exec(`chmod +x ${executable}`);
+    core.addPath(targetdir);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
-}
+};
 
-async function run(): Promise<void> {
+const run = async (): Promise<void> => {
   try {
-    const targetdir: string = core.getInput('dir') || ''.concat(process.env.RUNNER_WORKSPACE || '', '/bin')
-    await exec.exec(`mkdir -p ${targetdir}`)
+    const targetdir: string = core.getInput('dir') || ''.concat(process.env.RUNNER_WORKSPACE || '', '/bin');
+    await exec.exec(`mkdir -p ${targetdir}`);
     await install_target(
       'https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/',
       'linuxdeploy-x86_64.AppImage',
       targetdir
-    )
-    const plugins = core.getInput('plugins').split(' ')
+    );
+    const plugins = core.getInput("plugins").split(' ');
     if (plugins) {
       for (const currentValue of plugins) {
         switch (currentValue) {
@@ -34,7 +34,7 @@ async function run(): Promise<void> {
               'https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/',
               'linuxdeploy-plugin-qt-x86_64.AppImage',
               targetdir
-            )
+            );
             break
           }
           case 'conda': {
@@ -42,7 +42,7 @@ async function run(): Promise<void> {
               'https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/',
               'linuxdeploy-plugin-conda.sh',
               targetdir
-            )
+            );
             break
           }
           case 'python': {
@@ -50,7 +50,7 @@ async function run(): Promise<void> {
               'https://github.com/niess/linuxdeploy-plugin-python/releases/download/continuous/',
               'linuxdeploy-plugin-python-x86_64.AppImage',
               targetdir
-            )
+            );
             break
           }
           case 'appimage': {
@@ -58,15 +58,15 @@ async function run(): Promise<void> {
               'https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/',
               'linuxdeploy-plugin-appimage-x86_64.AppImage',
               targetdir
-            )
+            );
             break
           }
         }
       }
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
-}
+};
 
-run()
+run();
