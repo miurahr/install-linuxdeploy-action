dac9a624e874d1787bcd621f206a771d02fd4efe
Hiroshi Miura <miurahr@linux.com>
2021-04-10 11:10:02 +0900

    run Prettier
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
-import * as temp from 'temp';
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as temp from 'temp'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 const install_target = async (target_base, name, targetdir): Promise<void> => {
   try {
-    const target: string = target_base.concat(name);
+    const target: string = target_base.concat(name)
     core.info(`⬇Downloading ${name}...`)
-    const executable: string = targetdir.concat('/', name);
-    await exec.exec(`wget -c -nv ${target} -O ${executable}`);
-    await exec.exec(`chmod +x ${executable}`);
+    const executable: string = targetdir.concat('/', name)
+    await exec.exec(`wget -c -nv ${target} -O ${executable}`)
+    await exec.exec(`chmod +x ${executable}`)
     core.debug(`Downloaded to ${executable}`)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
-};
+}
 
 const run = async (): Promise<void> => {
   try {
-    const targetdir: string = core.getInput('dir') || temp.mkdirSync();
+    const targetdir: string = core.getInput('dir') || temp.mkdirSync()
     if (targetdir) {
-      await exec.exec(`mkdir -p ${targetdir}`);
-      core.addPath(targetdir);
+      await exec.exec(`mkdir -p ${targetdir}`)
+      core.addPath(targetdir)
     }
     await install_target(
       'https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/',
       'linuxdeploy-x86_64.AppImage',
-				targetdir
-    );
-    const plugins = core.getInput("plugins").split(' ');
+      targetdir
+    )
+    const plugins = core.getInput('plugins').split(' ')
     if (plugins) {
       for (const currentValue of plugins) {
         switch (currentValue) {
@@ -35,40 +35,40 @@ const run = async (): Promise<void> => {
             await install_target(
               'https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/',
               'linuxdeploy-plugin-qt-x86_64.AppImage',
-               targetdir
-            );
+              targetdir
+            )
             break
           }
           case 'conda': {
             await install_target(
               'https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/',
               'linuxdeploy-plugin-conda.sh',
-                targetdir
-            );
+              targetdir
+            )
             break
           }
           case 'python': {
             await install_target(
               'https://github.com/niess/linuxdeploy-plugin-python/releases/download/continuous/',
               'linuxdeploy-plugin-python-x86_64.AppImage',
-                targetdir
-            );
+              targetdir
+            )
             break
           }
           case 'appimage': {
             await install_target(
               'https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/',
               'linuxdeploy-plugin-appimage-x86_64.AppImage',
-                targetdir
-            );
+              targetdir
+            )
             break
           }
         }
       }
     }
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
-};
+}
 
-run();
+run()
