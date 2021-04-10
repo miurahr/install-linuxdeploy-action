8761294c3944f78816440f5a6218e12721163ac7
Hiroshi Miura <miurahr@linux.com>
2021-04-10 11:16:53 +0900

    Update release process
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- release.sh
+++ release.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-git checkout releases
-git merge -m 'merge master into releases' master
-npm install
 npm install -g typescript
 npm install -g webpack
 npm install -g @vercel/ncc
-npm run all
-git add action.yml dist/
+git checkout releases
+git merge -m 'merge master into releases' master
+npm run release
+git add action.yml dist/ -f
 git commit -sm 'Update release'
-git push origin releases
