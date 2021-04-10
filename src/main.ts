32fa2df9adf89fdc899a815d042a7f48e386b49e
Hiroshi Miura <miurahr@linux.com>
2021-04-10 09:44:28 +0900

    Change default install directory
    - Use @actions/tool-cache library to download
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import * as process from 'process';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as tc from '@actions/tool-cache';
 
-const install_target = async (target_base, name, targetdir): Promise<void> => {
+const install_target = async (target_base, name, optionDir): Promise<void> => {
   try {
     const target: string = target_base.concat(name);
-    const executable: string = targetdir.concat('/', name);
-    await exec.exec(`wget -c -nv ${target} -O ${executable}`);
-    await exec.exec(`chmod +x ${executable}`);
-    core.addPath(targetdir);
+    core.info(`⬇️ Downloading ${name}...`)
+    let executable: string;
+    if (optionDir) {
+      executable = optionDir.concat('/', name);
+      await exec.exec(`wget -c -nv ${target} -O ${executable}`);
+      await exec.exec(`chmod +x ${executable}`);
+      core.debug(`Downloaded to ${executable}`)
+    } else {
+  		executable = await tc.downloadTool(target)
+      await exec.exec(`chmod +x ${executable}`);
+      core.debug(`Downloaded to ${executable}`)
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -18,12 +24,15 @@ const install_target = async (target_base, name, targetdir): Promise<void> => {
 
 const run = async (): Promise<void> => {
   try {
-    const targetdir: string = core.getInput('dir') || ''.concat(process.env.RUNNER_WORKSPACE || '', '/bin');
-    await exec.exec(`mkdir -p ${targetdir}`);
+    const optionDir: string = core.getInput('dir');
+    if (optionDir) {
+      await exec.exec(`mkdir -p ${optionDir}`);
+      core.addPath(optionDir);
+    }
     await install_target(
       'https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/',
       'linuxdeploy-x86_64.AppImage',
-      targetdir
+				optionDir
     );
     const plugins = core.getInput("plugins").split(' ');
     if (plugins) {
@@ -33,7 +42,7 @@ const run = async (): Promise<void> => {
             await install_target(
               'https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/',
               'linuxdeploy-plugin-qt-x86_64.AppImage',
-              targetdir
+               optionDir
             );
             break
           }
@@ -41,7 +50,7 @@ const run = async (): Promise<void> => {
             await install_target(
               'https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/',
               'linuxdeploy-plugin-conda.sh',
-              targetdir
+                optionDir
             );
             break
           }
@@ -49,7 +58,7 @@ const run = async (): Promise<void> => {
             await install_target(
               'https://github.com/niess/linuxdeploy-plugin-python/releases/download/continuous/',
               'linuxdeploy-plugin-python-x86_64.AppImage',
-              targetdir
+                optionDir
             );
             break
           }
@@ -57,7 +66,7 @@ const run = async (): Promise<void> => {
             await install_target(
               'https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/',
               'linuxdeploy-plugin-appimage-x86_64.AppImage',
-              targetdir
+                optionDir
             );
             break
           }
